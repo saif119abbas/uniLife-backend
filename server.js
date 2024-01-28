fb7062332b57bcf200138f122e04d4fe3ca6c47c
saifadabbas <s11926449@stu.najah.edu>
2024-01-28 22:21:25 +0200

    edited for online server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {
   sendMessage,
   sendImage,
   seenMessage,
-} = require("./SocketMethods/createMessage");
+} = require("./socketMethods/createMessage");
 
 const io = new Server(server, {
   cors: {
